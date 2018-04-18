7ed21c2924221824a9ef98dc894ca5e844634789
Phillip Surrett <dervareal90@mail.com>
2018-04-18 18:22:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function sayHiToGrandma(string) {
   return ("I can't hear you!")
  else(string.toUpperCase() === string)
 return("YES INDEED!")
-else if(string === "I love you")
+elseIf(string === "I love you")
 return("I love you, too.")
 }
