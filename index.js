a491824883e7e567f8cd2b127e0139d60147fcf6
Phillip Surrett <dervareal90@mail.com>
2018-04-18 18:19:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ function shout(string) {
 function whisper(string) {
  return string.toLowerCase()
 }
-function logshout(string) {
-  console.log(string.toUpperCase)
+
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase() === string)
+  return ("I can't hear you!")
+} else(string.toUpperCase() === string)
+return("YES INDEED")
+
+
